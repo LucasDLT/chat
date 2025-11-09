7ceac5a2d367ca6884d947bf20e296e2cb1a788c
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-11-08 23:56:25 -0300

    feat:integramos ping-pong y limpieza de interval en evento close de wss

--- server/src/websoquet.setup.ts
+++ server/src/websoquet.setup.ts
@@ -3,14 +3,32 @@ import type { Server } from 'http';
 import type { ClientToServerMessage, ErrorMessage, ServerToClientMessage } from './types/message.t';
 
 
-
+//funcion que saque de la documentacion en github, para el ping-pong
+function heartbeat(this:WebSocket){
+    this.isAlive=true
+}
 export const websocketSetup = (server: Server) => {
     const wss = new WebSocketServer({server});
     
     
     wss.on("connection", (ws:WebSocket)=>{
-
+        ws.isAlive=true
+        
+        ws.on("error", (error:Error)=>{
+            console.log(`error de conexion ${error.message}, tipo: ${error.name}, ubucacion-. ${error.stack}`)
+        })
+        ws.on("pong", heartbeat)
     })
+    //continuamos con la funcion del ping-pong. Limpiamos cada 30s de inactividad y verificamos el isAlive como el readyState
+    const interval:NodeJS.Timeout = setInterval(function ping(){
+    wss.clients.forEach( function each(client:WebSocket){
+            if (client.isAlive===false ) return client.terminate()
+            if (client.readyState !== WebSocket.OPEN) return client.terminate()
+        
+            client.isAlive=false
+            client.ping()
+        })
+    },30000)
 
     //aca dejamos errores y cierre del servidor websocket, sin ws ya que para esta altura no deberian quedar conexiones activas
     wss.on("error", (error)=>{
@@ -19,5 +37,6 @@ export const websocketSetup = (server: Server) => {
 
     wss.on("close",()=>{
         console.log("close del wss");
+        clearInterval(interval)
     })
 }
