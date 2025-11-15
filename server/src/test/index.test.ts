1ceb1fcd442059bed1614873728a2d1670b7df4d
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-11-15 17:54:32 -0300

    test: integramos test del guard isRegisterNickname

--- server/src/test/index.test.ts
+++ server/src/test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
-import { isSendMessage } from "../guards";
+import { isSendMessage, isRegisterNickname } from "../guards";
 
 describe("isSendMessage guard", () => {
   test("Should return true for a valid public SendMessage", () => {
@@ -33,7 +33,7 @@ describe("isSendMessage guard", () => {
   test("Should return false if messageId is missing", () => {
     const missingId = {
       type: "chat.send",
-      timestamp:Date.now(),
+      timestamp: Date.now(),
       payload: {
         scope: "chat.public",
         toId: "toid",
@@ -47,7 +47,7 @@ describe("isSendMessage guard", () => {
     const invalidScope = {
       type: "chat.send",
       messageId: "msg-id",
-      timestamp:Date.now(),
+      timestamp: Date.now(),
       payload: {
         scope: "chat",
         toId: "toid",
@@ -61,7 +61,7 @@ describe("isSendMessage guard", () => {
     const invalidType = {
       type: "chat",
       messageId: "msg-id",
-      timestamp:Date.now(),
+      timestamp: Date.now(),
       payload: {
         scope: "chat.public",
         toId: "toid",
@@ -74,7 +74,7 @@ describe("isSendMessage guard", () => {
   test("Should return false if payload missing", () => {
     const missingPayload = {
       type: "chat.send",
-      timestamp:Date.now(),
+      timestamp: Date.now(),
       messageId: "msg-id",
     };
     expect(isSendMessage(missingPayload)).toBe(false);
@@ -83,7 +83,7 @@ describe("isSendMessage guard", () => {
   test("Should return false if type missing", () => {
     const missingType = {
       messageId: "msg-id",
-      timestamp:Date.now(),
+      timestamp: Date.now(),
       payload: {
         scope: "chat.public",
         toId: "toid",
@@ -96,8 +96,8 @@ describe("isSendMessage guard", () => {
   test("Should return false if invalid text in payload", () => {
     const invalidText = {
       type: "chat.send",
-      messageId: "msg-id",      
-      timestamp:Date.now(),
+      messageId: "msg-id",
+      timestamp: Date.now(),
       payload: {
         scope: "chat.public",
         toId: "toid",
@@ -107,16 +107,130 @@ describe("isSendMessage guard", () => {
     expect(isSendMessage(invalidText)).toBe(false);
   });
 
-  test("Should return false if missing date",()=>{
-     const invalidText = {
+  test("Should return false if missing date", () => {
+    const invalidText = {
       type: "chat.send",
-      messageId: "msg-id",      
+      messageId: "msg-id",
       payload: {
         scope: "chat.public",
         toId: "toid",
         text: "missing date",
       },
     };
-    expect(isSendMessage(invalidText)).toBe(false)
+    expect(isSendMessage(invalidText)).toBe(false);
+  });
+});
+
+describe("isRegisterNickname guard", () => {
+  test("Should return true for valid registerNickname", () => {
+    const validMsg = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        messageId: "msgId",
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(validMsg)).toBeTruthy();
+  });
+
+  test("Should return false if missing timestamp",()=>{
+    const missingTimestamp = {
+      type: "registerNickname",
+      payload: {
+        messageId: "msgId",
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(missingTimestamp)).toBeFalsy()
+  });
+
+  test("Should return false if invalid type date", ()=>{
+    const invalidData = {
+      type: "registerNickname",
+      timestamp: "data string",
+      payload: {
+        messageId: "msgId",
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(invalidData)).toBeFalsy()
   })
+
+  test("Should return false if invalid type", () => {
+    const invalidType = {
+      type: "invalid",
+      timestamp: Date.now(),
+      payload: {
+        messageId: "msgId",
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(invalidType)).toBeFalsy();
+  });
+
+  test("Should return false if missing type", () => {
+    const missingType = {
+      timestamp: Date.now(),
+      payload: {
+        messageId: "msgId",
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(missingType)).toBeFalsy();
+  });
+
+  test("Should return false if missing payload", () => {
+    const missingPayload = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+    };
+    expect(isRegisterNickname(missingPayload)).toBeFalsy();
+  });
+
+  test("Should return false if invalid payload messageId", () => {
+    const invalidMsgId = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        messageId: 1234,
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(invalidMsgId)).toBeFalsy();
+  });
+
+  test("Should return false if missing messageId in payload", () => {
+    const missingMsgId = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        nickname: "nick",
+      },
+    };
+    expect(isRegisterNickname(missingMsgId)).toBeFalsy();
+  });
+
+  test("Should return false if invalid Nick", () => {
+    const invaliNick = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        messageId: "msgId",
+        nickname: 1234,
+      },
+    };
+    expect(isRegisterNickname(invaliNick)).toBeFalsy();
+  });
+
+  test("Should return false if missing nick in payload", () => {
+    const missingNick = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        messageId: "msgId",
+      },
+    };
+    expect(isRegisterNickname(missingNick)).toBeFalsy();
+  });
 });
