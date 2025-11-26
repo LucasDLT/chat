9430349ed8518217f6282300c4fd69f9778c716f
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-11-25 21:52:59 -0300

    feat:integramos types + inicio de control de errores

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,32 @@
 "use client";
-import { timeStamp } from "console";
+import { RegisterNickname, SendMessage, ServerToClientMessage } from "@/types/types";
 import { FormEvent, useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const port = process.env.NEXT_PUBLIC_WS_PORT;
   const socketRef = useRef<WebSocket | null>(null);
   const [inputRegister, setInputRegister] = useState<string | undefined>("");
+  const [messageFeed, setMessageFeed] = useState<string[] | undefined>([""]);
+  const [inputMsg, setInputMsg] = useState<string | undefined>("");
 
   useEffect(() => {
     const socket = new WebSocket(`${port}`);
     socketRef.current = socket;
     socketRef.current.addEventListener("open", (event) => {
-      console.log("Connected to server");
+      setMessageFeed(["Conectado al servidor"]);
     });
     socketRef.current.addEventListener("message", (event) => {
-      console.log(event.data);
+      try {
+        const raw = event instanceof Buffer ? event.toString() : String(event);
+        const messageData:ServerToClientMessage = JSON.parse(raw);
+
+        if (messageData.type !== "ack") {
+          setMessageFeed((messageData) => [...(messageData ?? []), event.data]);
+          console.log(event.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     });
     socketRef.current.addEventListener("close", (event) => {
       console.log("Disconnected from server");
@@ -29,25 +41,48 @@ export default function Home() {
 
   const registerNick = (event: FormEvent) => {
     event.preventDefault();
-    const messageId=crypto.randomUUID()
-    const registerNick={
-      type:"registerNickname",
-      timestamp:Date.now(),
-      payload:{
-        messageId:messageId,
-        nickname:inputRegister
-      }
-
-    }
-    console.log({registerNick});
-    socketRef.current?.send(JSON.stringify(registerNick))
-    setInputRegister("");
-
+    const messageId = crypto.randomUUID();
+if(inputRegister){    const registerNick:RegisterNickname = {
+      type: "registerNickname",
+      timestamp: Date.now(),
+      payload: {
+        messageId: messageId,
+        nickname: inputRegister,
+      },
+    };
+    console.log({ registerNick });
+    socketRef.current?.send(JSON.stringify(registerNick));
+    setInputRegister("");}
   };
   const changeRegisterNick = (event: FormEvent) => {
-    event.preventDefault();
     const data = event.currentTarget as HTMLInputElement;
     setInputRegister(data.value);
+  };
+
+  const sendMessage = (event: FormEvent) => {
+    event.preventDefault();
+    const messageId = crypto.randomUUID();
+    if(inputMsg){
+    const message:SendMessage = {
+      timestamp: Date.now(),
+      type: "chat.send",
+      messageId: messageId,
+      payload: {
+        scope: "chat.public",
+        text: inputMsg,
+      },
+    };
+    socketRef.current?.send(JSON.stringify(message));
+    setInputMsg("");
+    setMessageFeed((messageFeed) => [
+      ...(messageFeed ?? []),
+      JSON.stringify(message),
+    ]);
+  }
+  };
+  const changeInputMessage = (event: FormEvent) => {
+    const data = event.currentTarget as HTMLInputElement;
+    setInputMsg(data.value);
   };
   return (
     <div className="flex flex-col justify-center items-center">
@@ -62,10 +97,24 @@ export default function Home() {
           className="border border-amber-100"
           onChange={changeRegisterNick}
           value={inputRegister}
-          
         />
-        <button  className="border rounded p-1 m-1">
-          registrar
+        <button className="border rounded p-1 m-1">registrar</button>
+        <section className="border h-[70vh] w-[50vw]">
+          <p>{messageFeed}</p>
+        </section>
+      </form>
+      <form
+        onSubmit={sendMessage}
+        className="flex items-center bg-amber-50 rounded mt-1"
+      >
+        <input
+          onChange={changeInputMessage}
+          value={inputMsg}
+          type="text"
+          className="border m-1 rounded bg-neutral-800 text-white p-1"
+        />
+        <button className="border m-1 rounded bg-neutral-800 text-white text-sm p-1.5 hover:cursor-pointer">
+          enviar
         </button>
       </form>
     </div>
