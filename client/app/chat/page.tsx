88540c467ba7b0f079284722fe582811566eed4e
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-12-11 22:47:42 -0300

    client-modularizacion: feed modularizado e integrado al chat.page

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -5,7 +5,7 @@ import { useAppContextWs } from "@/context/context";
 import { ClientsConected } from "@/types/types";
 import { DirectorySection } from "@/components/DirectorySection";
 import { NavbarChat } from "@/components/NavbarChat";
-import Image from "next/image";
+import { FeedSection } from "@/components/FeedComponent";
 
 export default function Chat() {
   const router = useRouter();
@@ -59,51 +59,14 @@ export default function Chat() {
         handleSelectClient={handleSelectClient}
         onClick={returnToGroup}
       />
-      <section className={`${activeFeed ? "flex yellowBg justify-center items-center h-100vh " : "hidden"}`}>
-        {privateIdMsg ? (
-          <section className={`h-[65vh] w-95 top-46 xl:h-[90vh] xl:w-[80vw]  absolute xl:top-1 xl:right-16.5 rounded-bl-xs rounded-br-xs ${activeMobile?"xl:w-[55vw]":""}`}>
-            <Image
-              src={'/background-directorio.jpg'}
-              width={3000}
-              height={1000}
-              alt="background feed chat"
-              className={`w-full h-full object-cover z-0 rounded-xs`}
-            ></Image>
-            <h3 className="absolute top-5 left-25 xl:top-0 rounded-b-sm bg-yellow-50/25 tracking-wider  xl:left-56 flex justify-center items-center w-60 xl:w-[50vw] h-10 text-2xl mesoninaRegular backdrop-blur-xl z-10">{clientSelected}</h3>
-            <div className="grid grid-cols-1 gap-2 z-10">
-              {messageFeedPriv.map((msg, index) => {
-                return (
-                  <p key={index} className="border bg-gray-800">
-                    {msg}
-                  </p>
-                );
-              })}
-            </div>
-          </section>
-        ) : (
-          //section para el feed de mensajes publicos
-          <section className=" h-[65vh] w-95 top-46 xl:h-[90vh] xl:w-[80vw]  absolute xl:top-1 xl:right-16.5 rounded-sm">
-            <Image
-              src={'/background-directorio.jpg'}
-              width={3000}
-              height={1000}
-              alt="background feed chat"
-              className={`w-full h-full object-cover z-0 rounded-xs`}
-            ></Image>
-            <h3 className="absolute top-5 left-25 xl:top-0 rounded-b-sm bg-yellow-50/25 tracking-wider xl:left-56 flex justify-center items-center w-60 xl:w-[50vw] h-10 text-2xl mesoninaRegular backdrop-blur-xl z-10">mensaje publico</h3>
-            <div className={` flex flex-col items-center overflow-y-auto h-[40vh] xl:h-[84vh] absolute g-2 top-14 xl:top-11  w-94 xl:w-[79vw]`}>
-              {messageFeed.map((msg, index) => {
-                return (
-                  <p key={index} className="text-center mt-2 xl:mt-2 ">
-                    {msg}
-                  </p>
-                );
-              })}
-            </div>
-          </section>
-        )}
-      </section>
 
+      <FeedSection
+        activeFeed={activeFeed}
+        privateIdMsg={privateIdMsg}
+        messageFeed={messageFeed}
+        messageFeedPriv={messageFeedPriv}
+        clientSelected={clientSelected}
+      />
       <NavbarChat
         active={active}
         activeMobile={activeMobile}
---
+++ client/components/FeedComponent/index.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+
+interface FeedProps {
+  activeFeed: boolean;
+  privateIdMsg: string | undefined;
+  messageFeed:string[];
+  messageFeedPriv:string[];
+  clientSelected:string|undefined;
+
+}
+
+export const FeedSection: React.FC<FeedProps> = ({
+  activeFeed,
+  privateIdMsg,
+  messageFeed,
+  messageFeedPriv,
+  clientSelected
+}) => {
+  return (
+    <section
+      className={`${
+        activeFeed
+          ? "flex yellowBg justify-center items-center h-100vh "
+          : "hidden"
+      }`}
+    >
+      {privateIdMsg ? (
+        <section
+          className={`h-[65vh] w-95 top-46 xl:h-[90vh] xl:w-[80vw]  absolute xl:top-1 xl:right-16.5 rounded-bl-xs rounded-br-xs 
+          `}
+        >
+          <Image
+            src={"/background-directorio.jpg"}
+            width={3000}
+            height={1000}
+            alt="background feed chat"
+            className={`w-full h-full object-cover z-0 rounded-xs`}
+          ></Image>
+          <h3 className="absolute top-5 left-25 xl:top-0 rounded-b-sm bg-yellow-50/25 tracking-wider  xl:left-56 flex justify-center items-center w-60 xl:w-[50vw] h-10 text-2xl mesoninaRegular backdrop-blur-xl z-10">
+            {clientSelected}
+          </h3>
+          <div className="grid grid-cols-1 gap-2 z-10">
+            {messageFeedPriv.map((msg, index) => {
+              return (
+                <p key={index} className="border bg-gray-800">
+                  {msg}
+                </p>
+              );
+            })}
+          </div>
+        </section>
+      ) : (
+        //section para el feed de mensajes publicos
+        <section className=" h-[65vh] w-95 top-46 xl:h-[90vh] xl:w-[80vw]  absolute xl:top-1 xl:right-16.5 rounded-sm">
+          <Image
+            src={"/background-directorio.jpg"}
+            width={3000}
+            height={1000}
+            alt="background feed chat"
+            className={`w-full h-full object-cover z-0 rounded-xs`}
+          ></Image>
+          <h3 className="absolute top-5 left-25 xl:top-0 rounded-b-sm bg-yellow-50/25 tracking-wider xl:left-56 flex justify-center items-center w-60 xl:w-[50vw] h-10 text-2xl mesoninaRegular backdrop-blur-xl z-10">
+            mensaje publico
+          </h3>
+          <div
+            className={` flex flex-col items-center overflow-y-auto h-[40vh] xl:h-[84vh] absolute g-2 top-14 xl:top-11  w-94 xl:w-[79vw]`}
+          >
+            {messageFeed.map((msg, index) => {
+              return (
+                <p key={index} className="text-center mt-2 xl:mt-2 ">
+                  {msg}
+                </p>
+              );
+            })}
+          </div>
+        </section>
+      )}
+    </section>
+  );
+};
