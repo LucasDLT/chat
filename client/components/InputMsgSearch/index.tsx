a230776270e092df30a7562f17b1ddd0904d71dd
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-12-12 15:58:50 -0300

    client: se ubico al inputsearchmsgy debuggeo. Se encuentra funcional. Resta vista movil junto al inputsendmsg

--- client/components/InputMsgSearch/index.tsx
+++ client/components/InputMsgSearch/index.tsx
@@ -1,6 +1,5 @@
 import { ChangeEvent, FormEvent } from "react";
 import Image from "next/image";
-import { on } from "events";
 
 interface InputMsgSearchProps {
 onChange: (e:ChangeEvent<HTMLInputElement>) => void;
@@ -11,7 +10,7 @@ handleSearchMsg:(e: React.FormEvent<HTMLFormElement>) => void;
 export const InputMsgSearch:React.FC<InputMsgSearchProps> = ({onChange, inputMsgSearch, handleSearchMsg}) => {
   return (
     <form
-      className={``}
+      className={`z-11 xl:w-56 xl:h-9 xl:flex xl:justify-center xl:items-center absolute xl:right-78 xl:top-[5.6px]`}
       onSubmit={handleSearchMsg}
     >
       {/*NOTA: todavia no, pero en un momento voy a agregar aca una condicion con un estado que detecte en que modo de pantalla estamos y en que modo de feed estamos, dependendiendo de eso, este form va a ser para filtrar entre contactos o entre los mensajes del contacto seleccionado o del chat publico*/}
@@ -19,7 +18,7 @@ export const InputMsgSearch:React.FC<InputMsgSearchProps> = ({onChange, inputMsg
         onChange={onChange}
         type="text"
         className=" yellowBg h-6 rounded-xs text-black px-px text-center w-full mx-1 xl:w-45 xl:h-7"
-        value={inputMsgSearch}
+        value={inputMsgSearch? inputMsgSearch : ""}
       />
       <Image
         alt="icon user"
