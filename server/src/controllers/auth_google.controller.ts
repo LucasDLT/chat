a7c26ca7cbd82ad937912a83f6d3777ab3e01656
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-12-26 18:41:36 -0300

    server-controller-auth: controller finalizado con seteo de cookies y redireccion

--- server/src/controllers/auth_google.controller.ts
+++ server/src/controllers/auth_google.controller.ts
@@ -3,11 +3,13 @@ import { service_auth_google } from "../services/auth_google.service";
 
 export const auth_google = async (req: Request, res: Response) => {
   try {
-    const redirect_url = await service_auth_google();
-    //aca faltaria la logica para guardar el state en algun lugar
-    
-    res.redirect(redirect_url.url);
+    const {state, url} = await service_auth_google();
+
+    res.cookie("state", state, { httpOnly: true, sameSite: "lax", secure: false, maxAge: 5 * 60 * 1000 });//nota: eso son 5 min
+    res.status(200);
+    res.redirect(url);
+
   } catch (error) {
-    console.log(error);
+    res.status(500).send(error);// el error podria ser un objeto, de momento lo dejo asi y luego lo reviso. 
   }
 };
