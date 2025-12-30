b60603ebbc486ed75b7eb90863f1ce89ef9639f3
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-12-30 15:13:26 -0300

    ultimo push antes de v.2. Agregamos nuevas dependencias para typeORM

--- server/src/routes/route.login.ts
+++ server/src/routes/route.login.ts
@@ -1,6 +1,6 @@
 import {router} from "./router"
 import { login_controller } from "../controllers/login_controller"
 
-router.post("/login", login_controller)
+router.post("/auth/login", login_controller)
 
 export default router
