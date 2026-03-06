7c6121b13d3ac3262151fcff40a6a082d06ddbaa
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-03-06 12:25:30 -0300

    fix:verificacion UX para forms

--- client/app/components/ContainerForms/index.tsx
+++ client/app/components/ContainerForms/index.tsx
@@ -6,10 +6,12 @@ import { Aside_Message } from "@/app/ui/Aside_Form";
 import { forms } from "@/types/types";
 import { useRouter } from "next/navigation";
 import { resolve_request_me } from "@/helpers/me";
+import { useAppContextWs } from "@/context/context";
 
 export const Container_Forms = () => {
   const [activeForm, setActiveForm] = useState<forms>(forms.register);
   const router = useRouter();
+  const {setUser} = useAppContextWs();
   //verificacion para UX, si el usuario esta logueado lo redirecciona a /chat
   //no le permito estar logueado y acceder a register o login
   //el useeffect ejecuta resolve_request_me, esto devuelve un usuario, si es null lo dejo entrar a /forms pero si devuelve algo diferente a null lo redirecciona a /chat
@@ -18,6 +20,9 @@ export const Container_Forms = () => {
       const refresh_user = await resolve_request_me();
       if (refresh_user !== null) {
         router.push("/chat");
+      }else{
+        setUser(null);
+        router.push("/forms");
       }
     };
     veryfy_user();
